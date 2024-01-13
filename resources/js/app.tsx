b0ea72ebdc74d50a4bf1b25162ab4e482644e2f2
Joe McDowall <joe.mcdowall@gmail.com>
2024-01-13 10:57:52 +0000

    removing redundant hook and provider

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -10,7 +10,6 @@ import PageNotFound from "./pages/errors/PageNotFound";
 import Farm from "./pages/Farm";
 import Turbine from "./pages/Turbine";
 import Turbines from "./pages/Turbines";
-import { WindFarmProvider } from "./providers/WindFarmProvider";
 import Inspections from "./pages/Inspections";
 
 const app = document.getElementById("app") as HTMLElement;
@@ -23,29 +22,27 @@ root.render(
   <React.StrictMode>
     <HelmetProvider>
       <CacheProvider>
-        <WindFarmProvider>
-          <AsyncBoundary>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<App />}>
-                  <Route path="/" element={<Welcome />} />
-                  <Route path="farms">
-                    <Route path="" element={<Farms />} />
-                    <Route path=":id" element={<Farm />} />
-                  </Route>
-                  <Route path="turbines">
-                    <Route path="" element={<Turbines />} />
-                    <Route path=":id" element={<Turbine />} />
-                  </Route>
-                  <Route path="inspections">
-                    <Route path="" element={<Inspections />} />
-                  </Route>
+        <AsyncBoundary>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />}>
+                <Route path="/" element={<Welcome />} />
+                <Route path="farms">
+                  <Route path="" element={<Farms />} />
+                  <Route path=":id" element={<Farm />} />
                 </Route>
-                <Route path="*" element={<PageNotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </AsyncBoundary>
-        </WindFarmProvider>
+                <Route path="turbines">
+                  <Route path="" element={<Turbines />} />
+                  <Route path=":id" element={<Turbine />} />
+                </Route>
+                <Route path="inspections">
+                  <Route path="" element={<Inspections />} />
+                </Route>
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </AsyncBoundary>
       </CacheProvider>
     </HelmetProvider>
   </React.StrictMode>,
